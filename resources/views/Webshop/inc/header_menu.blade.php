a0ca71f3606a4e5ef75ec95c4f864168fe2d6e0a
Antonio Cariello <antonio@farwebsr.it>
2025-03-06 17:25:33 +0100

    plc search

--- resources/views/Webshop/inc/header_menu.blade.php
+++ resources/views/Webshop/inc/header_menu.blade.php
@@ -40,6 +40,12 @@
                 @endif
             </a>
 
+            {{--CERCA IN HEADER --}}
+            <div class="navbar-search d-none d-md-block">
+                @include("$thema.plugins.pluginProducts.v3.inc.search_top")
+            </div>
+            {{--CERCA IN HEADER --}}
+
             <div class="collapse navbar-collapse" id="navbar-main-collapse">
                 <div class="navbar-header">
                     <a class="navbar-brand-mobile" href="{{ url('/') }}">
@@ -69,10 +75,6 @@
                     </button>
                 </div>
 
-                <div class="header_searchbar">
-                    @include("$thema.plugins.pluginProducts.v3.inc.search_top")
-                </div>
-
                 @include('Webshop.inc.menu')
 
                 <div class="social-mobile">
@@ -94,18 +96,28 @@
                 @include('Webshop.plugins.pluginBooking.inc.topbar_menu')
             @endif
 
+            {{--CERCA IN HEADER --}}
+            <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#search-mobile" aria-expanded="false" aria-label="Cerca">
+                <i class="bi bi-search"></i>
+            </button>
+            {{--CERCA IN HEADER --}}
 
             <button class="navbar-toggler open-navbar" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main-collapse" aria-expanded="false" aria-label="Menu">
-                {{--<svg viewBox="0 0 100 80" width="24" height="24">
-                    <rect width="100" height="10"></rect>
-                    <rect y="30" width="100" height="10"></rect>
-                    <rect y="60" width="50" height="10"></rect>
-                </svg>--}}
                 <span class="lines"></span>
             </button>
 
         </nav>
     </div>
+
+    {{--CERCA IN HEADER --}}
+    <div class="d-block d-md-none">
+        <div class="collapse" id="search-mobile">
+            <div class="container-fluid">
+                @include("$thema.plugins.pluginProducts.v3.inc.search_top_mobile")
+            </div>
+        </div>
+    </div>
+    {{--CERCA IN HEADER --}}
 
     @yield('topbar_ecommerce')
 </header>
