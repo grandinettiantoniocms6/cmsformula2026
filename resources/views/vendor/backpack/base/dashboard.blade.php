82afb0d482211f1876d3113694af2ee0173aff6e
Keivan Tahami <keivan@farwebsrl.it>
2025-03-21 11:42:51 +0100

    NEWS

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -601,6 +601,7 @@
 
     @if(env('NEWS') == 1)
         <?php
+
             $url = "https://gest.webisland.it/news.xml";
             $xml = simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA);
         ?>
