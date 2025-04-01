c1086ec483312cdc8f95f4c1467385ebf18bcf56
Keivan Tahami <keivan@farwebsrl.it>
2025-04-01 20:08:51 +0200

    bug totali carrello e checkout

--- config/elfinder.php
+++ config/elfinder.php
@@ -76,6 +76,21 @@ return [
     |
     */
 
-    'options' => [],
+    'options' => [
+        "tmbPath" => "tmb"
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Root Options
+    |--------------------------------------------------------------------------
+    |
+    | These options are merged, together with every root by default.
+    | See https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options-2.1#root-options
+    |
+    */
+    'root_options' => [
+        "tmbPath" => "tmb"
+    ],
 
 ];
