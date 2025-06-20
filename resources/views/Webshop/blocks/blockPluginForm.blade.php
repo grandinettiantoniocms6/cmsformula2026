a65e28ded39a11026db42d628b04369d5381bd1f
Keivan <Keivantg@gmail.com>
2025-06-20 15:34:06 +0200

    fix bug

--- resources/views/Webshop/blocks/blockPluginForm.blade.php
+++ resources/views/Webshop/blocks/blockPluginForm.blade.php
@@ -4,7 +4,7 @@ $form = \App\Models\PluginForms::find($item->form_id); ?>
 <?php
 $title = $form->title_form;
 $subtitle = $form->subtitle_form;
-$fields = $form->content;
+$fields = json_decode($form->content, true);
 ?>
 <section id="formpro-{{ $item->form_id }}" class="block-formpro">
     <div class="container">
