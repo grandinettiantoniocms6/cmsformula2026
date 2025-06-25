f900f841bf1e46433b9734ae950d32b7c33e59f9
Keivan <Keivantg@gmail.com>
2025-06-25 23:19:52 +0200

    fix bug

--- resources/views/vendor/backpack/base/inc/blocks_loop.blade.php
+++ resources/views/vendor/backpack/base/inc/blocks_loop.blade.php
@@ -18,7 +18,7 @@ if($pages_blocks){
         ?>
         @if($item)
             <div id="header_row_{{ $pb->id }}" data-index="{{ $pb->id }}" data-position="{{ $pb->order }}">
-                @include('vendor.backpack.base.inc.block')
+
             </div>
         @endif
         <?php
