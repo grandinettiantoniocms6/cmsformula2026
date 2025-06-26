cdebf477383a93da473cbcb04373ad3106795434
Antonio Cariello <antonio@farwebsr.it>
2025-06-26 12:26:16 +0200

    fix dashboard news

--- resources/views/vendor/backpack/ui/dashboard.blade.php
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -1014,9 +1014,11 @@
                     @foreach($news as $new)
                         <div class="list-group-item">
                             <h5 class="mb-0" data-toggle="collapse" data-target="#collapse_{{ $i }}" aria-expanded="{{ $i == 1 ? 'true' : 'falso' }}">{{ $new->title }}</h5>
-                            <div id="collapse_{{ $i }} {{ $i == 1 ? 'show' : '' }}" class="collapse" data-parent="#accordion_news">
-                                {!! $new->description !!}
-                                <small>{{ \Carbon\Carbon::createFromFormat("Y-m-d H:i:s", $new->created_at)->format("d/m/Y") }}</small>
+                            <div id="collapse_{{ $i }}" class="collapse {{ $i == 1 ? 'show' : '' }}" data-parent="#accordion_news">
+                                <div class="pt-2">
+                                    {!! $new->description !!}
+                                    <small>{{ \Carbon\Carbon::createFromFormat("Y-m-d H:i:s", $new->created_at)->format("d/m/Y") }}</small>
+                                </div>
                             </div>
                         </div>
                         @php $i++; @endphp
