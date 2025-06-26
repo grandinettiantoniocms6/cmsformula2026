5b038c7b2995bf5abfc33d32557de894a693cd29
Keivan <Keivantg@gmail.com>
2025-06-26 10:23:01 +0200

    fix bug

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,12 +6,19 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+$vet = array_merge(
+    (array) config('backpack.base.web_middleware', 'web'),
+    (array) config('backpack.base.middleware_key', 'admin'),
+);
+
+if(env('OCTANE_HTTPS') == true){
+    $vet[] = "octane:disable";
+}
+
+
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
-    'middleware' => array_merge(
-        (array) config('backpack.base.web_middleware', 'web'),
-        (array) config('backpack.base.middleware_key', 'admin')
-    ),
+    'middleware' => $vet,
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
 
@@ -186,4 +193,8 @@ Route::group([
     Route::crud('plugin-interventions-note', 'PluginInterventionsNoteCrudController');
     Route::crud('plugin-labels', 'PluginLabelsCrudController');
     Route::crud('plugin-labels-settings', 'PluginLabelsSettingsCrudController');
+
+
+
 }); // this should be the absolute last line of this file
+
