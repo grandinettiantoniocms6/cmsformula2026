5dd749ae18b62fcf088eb3161fb3db870de58ae4
Keivan <Keivantg@gmail.com>
2025-06-26 10:08:56 +0200

    fix bug

--- resources/views/vendor/backpack/ui/dashboard.blade.php
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -997,7 +997,11 @@
     @if(env('LOCAL') == 0)
         <?php
             $gestDB = \DB::connection('mysql_2');
-            $news = $gestDB->table("news")->where("is_active", 1)->orderBy("lft", "asc")->take(5)->get();
+            $news = $gestDB->table("news")
+                ->whereNull("deleted_at")
+                ->where("is_active", 1)
+                ->orderBy("lft", "asc")
+                ->take(5)->get();
         ?>
 
         @if($news)
