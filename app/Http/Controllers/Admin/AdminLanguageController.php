63891b7bf2ca0c91ccef23f8713bf9dd1135d998
Keivan <Keivantg@gmail.com>
2025-07-16 23:41:46 +0200

    fix dev

--- app/Http/Controllers/Admin/AdminLanguageController.php
+++ app/Http/Controllers/Admin/AdminLanguageController.php
@@ -273,7 +273,7 @@ class AdminLanguageController extends \App\Http\Controllers\Controller
                     break;
 
                 case "blockPortfolio2":
-                    $fields = ["title",'description','url_interno','url','button'];
+                    $fields = ['title','description','url_interno','url','button'];
                     $fields_types = ["text", "text", "select2_from_array", "text", "text"];
                     $fields_label = ["Titolo", "Sottotitolo", "URL interno", "URL esterno", "Testo pulsante"];
 
@@ -1230,7 +1230,7 @@ class AdminLanguageController extends \App\Http\Controllers\Controller
                 $fields = ['title','description','url_interno','url','button'];
                 break;
             case "blockPortfolio2":
-                $fields = ['title','abstract','url_interno','url','button'];
+                $fields = ['title','description','url_interno','url','button'];
                 break;
 
             case "blockGallery":
