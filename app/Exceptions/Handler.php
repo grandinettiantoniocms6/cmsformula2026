2b5dc367335268fbab371e5912adf51152cefd46
Keivan <Keivantg@gmail.com>
2025-07-29 18:19:56 +0200

    fix dev

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,11 +48,16 @@ class Handler extends ExceptionHandler
                 "The given data was invalid.", "CSRF token mismatch.",
                 "The GET method is not supported for this route. Supported methods: PUT, DELETE.",
                 "The POST method is not supported for this route. Supported methods: GET, HEAD."];
+
             if(!in_array(trim($exception->getMessage()), $vet)){
-                if(!is_numeric(strpos($exception->getMessage(), "Supported methods")) && !is_numeric(strpos($exception->getMessage(), "not supported")) && !is_numeric(strpos($exception->getMessage(), "Unauthenticated"))){
+                if(!is_numeric(strpos($exception->getMessage(), "Supported methods"))
+                    && !is_numeric(strpos($exception->getMessage(), "not supported"))
+                    && !is_numeric(strpos($exception->getMessage(), "Unauthenticated"))
+                    && !is_numeric(strpos($exception->getMessage(), "The route"))
+                ){
                     try{
                         \Mail::send('common.emails.error', ['error' => $exception->getMessage(), 'file' => $exception->getFile(), 'line' => $exception->getLine(), 'store' => "", 'trace' =>  $exception->getTraceAsString(), 'url' => \Request::url()], function ($m) use ($exception) {
-                            $m->from("info@cmsformula5.it", "CMSFORMULA6");
+                            $m->from("info@cmsformula5.it", "CMSFORMULA 6");
                             $m->to('info@webisland.it', 'Webisland')
                                 ->cc('keivantg@gmail.com', 'Webisland')
                                 ->subject("BUG CMSFORMULA 6 {$exception->getMessage()}");
