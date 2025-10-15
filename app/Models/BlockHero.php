14b20b5fc1198ae1e178cff946132adcd9fc8b41
Keivan <Keivantg@gmail.com>
2025-10-15 11:55:05 +0200

    fix bug

--- app/Models/BlockHero.php
+++ app/Models/BlockHero.php
@@ -75,11 +75,11 @@ class BlockHero extends Model
                 $absolute = $this->resolveImagePath($value);
 
                 // Se è un URL remoto, meglio usare i byte/stream
-                if (Str::startsWith($absolute, ['http://', 'https://'])) {
+                if (\Str::startsWith($absolute, ['http://', 'https://'])) {
                     $bytes  = file_get_contents($absolute); // o Http::get(...)->body()
-                    $image  = Image::make($bytes)->encode('webp', 90);
+                    $image  = \Image::make($bytes)->encode('webp', 90);
                 } else {
-                    $image  = Image::make($absolute)->encode('webp', 90);
+                    $image  = \Image::make($absolute)->encode('webp', 90);
                 }
 
                 $width = $thumb->width_max != 0 ? $thumb->width_max : null;
