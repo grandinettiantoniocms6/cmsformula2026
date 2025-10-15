7c523c74299fdc255216298e64912ddf8d9362fc
Keivan <Keivantg@gmail.com>
2025-10-15 11:18:04 +0200

    fix bug

--- app/Http/Requests/BlockHeroRequest.php
+++ app/Http/Requests/BlockHeroRequest.php
@@ -26,10 +26,12 @@ class BlockHeroRequest extends FormRequest
     public function rules()
     {
         // qui metto solo i request dei campi presenti su modifica blocco (no campi multi)
-        if(is_numeric(strpos($_SERVER['QUERY_STRING'], "multi")) || ($_SERVER['REQUEST_METHOD'] == "POST" && is_numeric(strpos($_SERVER['HTTP_REFERER'], "multi")))){
-            return [
-                'name' => 'required'
-            ];
+        if(key_exists("QUERY_STRING", $_SERVER)){
+            if(is_numeric(strpos($_SERVER['QUERY_STRING'], "multi")) || ($_SERVER['REQUEST_METHOD'] == "POST" && is_numeric(strpos($_SERVER['HTTP_REFERER'], "multi")))){
+                return [
+                    'name' => 'required'
+                ];
+            }
         }
 
         // qui metto solo i request dei campi Multi presenti sull'inserimento dei record del blocco
