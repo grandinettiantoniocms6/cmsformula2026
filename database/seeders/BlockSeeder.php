7c3377222580d0b4f2a825950988868b8e0905e7
Keivan <Keivantg@gmail.com>
2025-10-30 10:34:57 +0100

    push branch production

--- database/seeders/BlockSeeder.php
+++ database/seeders/BlockSeeder.php
@@ -235,6 +235,7 @@ class BlockSeeder extends Seeder
             "is_ordinable" => 0
         ]);
 
+
         \App\Models\AdminBlock::firstOrCreate(["name" => "blockFaq"],[
             "label" => "Faq",
             "name" => "blockFaq",
