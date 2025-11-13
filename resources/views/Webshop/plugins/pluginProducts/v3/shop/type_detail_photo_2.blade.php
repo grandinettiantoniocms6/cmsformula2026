49cc514be7e0ea3bf456974bf80c410fc5650a55
Keivan <Keivantg@gmail.com>
2025-11-13 10:06:08 +0100

    plc

--- resources/views/Webshop/plugins/pluginProducts/v3/shop/type_detail_photo_2.blade.php
+++ resources/views/Webshop/plugins/pluginProducts/v3/shop/type_detail_photo_2.blade.php
@@ -4,7 +4,7 @@
             @if(count($images))
                 @foreach($images as $k=>$image)
                     <?php
-                    if(isset($images_isext[$k]) == 1){
+                    if($images_isext[$k] == 1){
                         $url = $image;
                     }else{
                         $basename = basename($image);
