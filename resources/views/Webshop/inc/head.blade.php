63e0751f6240026c2a82236edf922bff1ad73125
Antonio Grandinetti <info@webisland.it>
2026-02-09 18:42:22 +0100

    Fix list prodotti in una colonna su Mobile

--- resources/views/Webshop/inc/head.blade.php
+++ resources/views/Webshop/inc/head.blade.php
@@ -5,7 +5,7 @@ $labelSite = \App\Models\Label::get()->pluck("value", "key")->toArray();
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="author" content="CMS-Formula 5.0 by Webisland.it" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta charset="UTF-8">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
