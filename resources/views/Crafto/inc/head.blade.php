e6442ec184ed504186ad7718146c64b4126e6f2a
Antonio Grandinetti <info@webisland.it>
2026-05-05 22:29:55 +0200

    Crafto Seo 05-05-26 22:30 <meta name="viewport" content="width=device-width, initial-scale=1">

--- resources/views/Crafto/inc/head.blade.php
+++ resources/views/Crafto/inc/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="author" content="CMS-Formula 6.0 by Webisland.it" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--meta name="viewport" content="width=device-width,initial-scale=1.0" />-->
 <!-- Favicon -->
 @if($website->favicon)
