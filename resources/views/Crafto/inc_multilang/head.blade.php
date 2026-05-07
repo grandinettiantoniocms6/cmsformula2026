d6e9562a2ba7ecf3a923ee12b1f5ce83cfd92551
Antonio Grandinetti <info@webisland.it>
2026-05-07 18:59:31 +0200

    Fix css head Crafto 07-05-2026

--- resources/views/Crafto/inc_multilang/head.blade.php
+++ resources/views/Crafto/inc_multilang/head.blade.php
@@ -6,8 +6,6 @@
 @if($website->favicon)
 <link rel="shortcut icon" href="{{ url("$website->favicon") }}" />
 @endif
-<link rel="preconnect" href="https://cdnjs.cloudflare.com" crossorigin>
-<link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
 <!-- font -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" media="print" onload="this.onload=null;this.removeAttribute('media');">
 <noscript><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"></noscript>
@@ -15,7 +13,14 @@
 <link rel="stylesheet" href="{{ url("templates/Crafto/css/vendors.min.css") }}"/>
 <link rel="stylesheet" href="{{ url("templates/Crafto/css/icon.min.css") }}"/>
 <link rel="stylesheet" href="{{ url("templates/Crafto/css/style.css") }}"/>
-<link rel="stylesheet" href="{{ url("templates/Crafto/css/responsive.css") }}"/>
+<link rel="preload"
+      href="{{ url("templates/Crafto/css/responsive.css") }}"
+      as="style"
+      onload="this.onload=null;this.rel='stylesheet'">
+<noscript>
+    <link rel="stylesheet"
+          href="{{ url("templates/Crafto/css/responsive.css") }}">
+</noscript>
 <link rel="stylesheet" href="{{ url("css_custom/crafto_custom.css") }}"/>
 
 <!-- CSS Common Form contact -->
