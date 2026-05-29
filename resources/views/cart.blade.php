f030478fc2f8097dbe41f122ef3134a519496cd2
Keivan <Keivantg@gmail.com>
2026-05-29 21:43:09 +0200

    prova push

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -10,6 +10,7 @@
     @include("$thema.inc.meta")
 @endsection
 
+
 @if($website->is_online == 1 || backpack_user() || is_numeric(strpos(env('APP_URL'), "stage")))
     @section('topbar')
         @include("$thema.inc.topbar")
